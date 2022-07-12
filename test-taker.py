c240cd3015ec85b2f747acd8f26e619e2c75d2d1
Sausagem8y <87502439+Sausagem8y@users.noreply.github.com>
2022-07-12 16:24:29 -0600

    Add files via upload

---
+++ test-taker.py
@@ -0,0 +1,25 @@
+import pyautogui
+
+width, height = pyautogui.size()
+
+def testTaker():
+    pyautogui.click(319, 447)
+    pyautogui.moveTo(578, 409, 1)
+    pyautogui.click(578, 409)
+    pyautogui.click(1128, 711)
+    pyautogui.moveTo(1128, 711, 1)
+    pyautogui.click(1128, 711)
+    pyautogui.moveTo(507,330, 1)
+    pyautogui.click(507, 330)
+    pyautogui.click(1128, 711)
+    pyautogui.moveTo(1128, 711, 1)
+    pyautogui.click(1128, 711)
+    pyautogui.moveTo(451, 422, 1)
+    pyautogui.click(451, 422)
+    pyautogui.click(1128, 711)
+    pyautogui.moveTo(1128, 711, 1)
+    pyautogui.click(1128, 711)
+   
+    
+
+testTaker()
